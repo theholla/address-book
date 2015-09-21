70d911148653418898f2edc0a91460bdefb768de
mlmendeznunez & Diana Holland <student@epicodus.com>
2015-09-21 10:02:20 -0700

    Add initial files

---
+++ README.md
@@ -0,0 +1,39 @@
+# Address Book
+
+#### By Marie L. Mendez-Nunez and Diana Holland
+
+## Description
+This web page creates an Address Book that allows us to enter our contacts' first and last names and addresses. Each new contact will be added to our contacts list.
+
+## Setup
+* To run this web page, just download files locally and then open address-book.html in browser.
+* To run tests, use: open/spec-runner.html
+* Depends on: Bootstrap, Mocha, Chai and JQuery
+
+## Technologies Used
+
+Jquery, JavaScript, HTML/CSS, Mocha, Bootstrap, Chai
+
+### Legal
+
+Copyright (c) 2015 ** Marie L. Mendez-Nunez and Diana Holland **
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+/* Form to add an additional address to a contact */
+  $("#add-address").click(function() {
+      $("#new-address").append('<div class="new-address">' +
+                                  '<div class="form-group">' +
+                                  '<label for="new-street">Street</label>'+
+                                  '<input type="text" class="form-control new-street">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-city">City</label>' +
+                                    '<input type="text" class="form-control new-city">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-state">State</label>' +
+                                    '<input type="text" class="form-control new-state">' +
+                                   '</div>' +
+                                '</div>');
+  });
+
+/* Form to add a contact */
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+
+    /* Add a contact's address */
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+
+    newContact.addresses.push(newAddress);
+  });
+/*Routing new contact information to web site*/
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " +       address.state + "</li>");
+      });
+    });
+
+    /* Form to add second address/edit a contact's info */
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+
+  });
+});
