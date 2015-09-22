18628d8f3cd35a5148ddcb45cce5d5354dd1b4e3
the holla <holla@bikefarm.org>
2015-09-22 08:36:18 -0700

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+function Contact(firstName,lastName){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+/* Clears the form after user clicks submit */
+function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+}
+
 $(document).ready(function() {
 
 /* Form to add an additional address to a contact */
@@ -24,7 +53,7 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     /* Add a contact's address */
     $(".new-address").each(function() {
@@ -36,18 +65,8 @@ $(document).ready(function() {
       newContact.addresses.push(newAddress);
     });
 
-    /* Hidden form to add a second address or update information */
-    $(".second-address").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var secondAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
-
-    newContact.addresses.push(secondAddress);
-    });
-
     /*Routing new contact information to web site*/
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName + "</span></li>");
 
 
     /* Show information for the contact that is clicked */
@@ -58,17 +77,14 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
 
       $("ul#addresses").text("");
+
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " +       address.state + "</li>");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
 
     /* Clears the form after the contact is added */
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
 
-  });//closes form new-contact
+  });
 });
